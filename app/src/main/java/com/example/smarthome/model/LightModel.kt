92358422c37e168d9bf33eff81a3b7a0043e2fa6
kunjangsherpa <kunjangsherpa07@email.com>
2025-12-12 13:15:31 +0545

    Synced with firebase

---
+++ app/src/main/java/com/example/smarthome/model/LightModel.kt
@@ -0,0 +1,8 @@
+package com.example.smarthome.model
+
+data class LightModel(
+    val light1On: Boolean = true,
+    val light2On: Boolean = true,
+    val light1Brightness: Float = 50f,
+    val light2Brightness: Float = 50f
+)
