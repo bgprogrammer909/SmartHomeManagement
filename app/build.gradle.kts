56a0df6270398e8106e4fbebbbf01d3a420524f1
pooja-pandey15 <poojapandeypp031@gmail.com>
2025-12-22 20:57:34 +0545

    Admin's add user UI done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation("com.cloudinary:cloudinary-android:2.1.0")
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
 
     // Firebase Database
     implementation("com.google.firebase:firebase-database-ktx:20.3.1")
