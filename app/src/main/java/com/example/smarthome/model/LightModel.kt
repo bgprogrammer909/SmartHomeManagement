3a6839fae4f36b5be2b5ab1b45303e154746318f
kunjangsherpa <kunjangsherpa07@gmail.com>
2025-12-30 04:02:07 +0545

    fixed logcat error

--- app/src/main/java/com/example/smarthome/model/LightModel.kt
+++ app/src/main/java/com/example/smarthome/model/LightModel.kt
@@ -5,7 +5,8 @@ data class LightModel(
     val light1On: Boolean = false,       // Light 1 on/off
     val light2On: Boolean = false,       // Light 2 on/off
     val light1Brightness: Float = 50f,   // Light 1 brightness (0-100)
-    val light2Brightness: Float = 50f    // Light 2 brightness (0-100)
+    val light2Brightness: Float = 50f,    // Light 2 brightness (0-100)
+    val isOn: Boolean = true
 ) {
     // Computed property to count how many lights are ON
     val lightsOnCount: Int
