8ce5928d3fa20f9303528cb78358d65e9fc26763
suchit <suchitbajracharya1@gmail.com>
2026-01-08 20:19:56 +0545

    water repo and implementation made and fixed modek

--- app/src/main/java/com/example/smarthome/viewmodel/WaterViewModel.kt
+++ app/src/main/java/com/example/smarthome/viewmodel/WaterViewModel.kt
@@ -3,7 +3,6 @@ package com.example.smarthome.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.smarthome.model.WaterModel
-import com.example.smarthome.repo.WaterRepo
 import com.example.smarthome.repo.WaterRepoImpl
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
