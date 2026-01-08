cf865fda08cbed87dd385455702acfdcc85fa9c7
pooja-pandey15 <poojapandeypp031@gmail.com>
2026-01-08 16:20:25 +0545

    loginScreenTest

---
+++ app/src/androidTest/java/com/example/smarthome/LoginScreenTest.kt
@@ -0,0 +1,26 @@
+package com.example.smarthome
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.smarthome.view.LoginActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LoginScreenTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<LoginActivity>()
+
+    @Test
+    fun loginScreen_isDisplayed() {
+        composeTestRule
+            .onNodeWithText("Sign in")
+            .assertIsDisplayed()
+    }
+
+}
